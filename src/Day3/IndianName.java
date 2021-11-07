48a60030f20a4464fc72786323800d991ee4b725
Moon <raining90@naver.com>
2021-11-07 19:27:17 +0900

    week1 학습일지 추가

--- src/Day3/IndianName.java
+++ src/Day3/IndianName.java
@@ -25,8 +25,13 @@ public class IndianName {
         String[] month = {"늑대", "태양", "양", "매", "황소", "불꽃", "나무", "달빛", "말", "돼지", "하늘", "바람"};
         String[] day = {"와 함께 춤을", "의 기상", "은(는) 그림자속에", "", "", "", "의 환생", "의 죽음", "아래에서", "를(을) 보라", "이(가) 노래하다", "그림자", "의 일격", "에게 쫓기는 남자", "의 행진", "의 왕", "의 유령", "을(를) 죽인 자", "은(는) 맨날 잠잔다", "처럼", "의 고향", "의 전사", "은(는) 나의 친구","의 노래", "의 정령", "의 파수꾼", "의 악마", "와(과) 같은 사나이", "을(를) 쓰러트린 자", "의 혼", "은(는) 말이 없다"};
 
-        System.out.println("당신의 이름은 " + year[y] + " " + month[m-1] + day[d-1] +"입니다.");
-
+        if(m > month.length){
+            System.out.println("12월까지 입력 가능합니다.");
+        }else if(d > day.length){
+            System.out.println("31일까지 입력 가능합니다.");
+        }else {
+            System.out.println("당신의 이름은 " + year[y] + " " + month[m - 1] + day[d - 1] + "입니다.");
+        }
     }
 
     public int splitYear(String year){
